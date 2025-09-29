13baa3b25d4bf8038c5628c0ed1c1894d095a608
Syed Furquan Ahmad <syed@nextdev.in>
2025-09-29 13:11:30 +0530

    Use gitsigns for current line blame

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -13,6 +13,15 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 1000,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+				use_focus = true,
+			},
 		},
 	},
 	{
@@ -622,23 +631,6 @@ return {
 			damping = 0.95, -- 0.85     [0, 1]
 			damping_insert_mode = 0.95, -- 0.9      [0, 1]
 			distance_stop_animating = 0.5, -- 0.1      > 0
-		},
-	},
-	{
-		"f-person/git-blame.nvim",
-		-- load the plugin at startup
-		event = "VeryLazy",
-		-- Because of the keys part, you will be lazy loading this plugin.
-		-- The plugin will only load once one of the keys is used.
-		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
-		-- or lazy = false. One of both options will work.
-		opts = {
-			-- your configuration comes here
-			-- for example
-			enabled = true, -- if you want to enable the plugin
-			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
-			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 		},
 	},
 }
