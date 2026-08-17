1489aa42cec51353a7e8f03300204d2219ca3ac6
Syed Furquan Ahmad <syed@nextdev.in>
2026-08-17 22:45:48 +0530

    nvim - add astro formatter and prettier fallback

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -564,14 +564,15 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
+
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				javascript = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				javascriptreact = { "prettierd" },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				json = { "prettierd", "prettier", stop_after_first = true },
+				astro = { "prettierd", "prettier", stop_after_first = true },
+
 				sh = { "shfmt" },
 			},
 		},
